741e08e22ae7bd1cc30a303fc8ae24c10933e2cf
Luis Eduardo <luiseduardosilva189@gmail.com>
2022-09-05 22:06:15 -0300

    fix: database dialect

--- app/models/db.js
+++ app/models/db.js
@@ -2,11 +2,11 @@
 const Sequelize = require('sequelize');
 
 const {
-  DB_NAME, DB_HOST, DB_PASS, DB_USER,
+  DB_DRIVER, DB_NAME, DB_HOST, DB_PASS, DB_USER,
 } = process.env;
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
-  dialect: 'mysql',
+  dialect: DB_DRIVER ?? 'mysql',
   define: {
     underscore: true,
     underscoreAll: true,
